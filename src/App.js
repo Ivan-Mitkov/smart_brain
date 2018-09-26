a70f7fa7d9c30b09af22a6986149401ddb9d6ded
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-09-26 21:13:52 +0300

    add Navigation and Logo components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Navigation />
+        <Logo />
+        {/* <ImageLinkForm />
+        <FaceRecognition /> */}
       </div>
     );
   }
